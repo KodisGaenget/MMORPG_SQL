06592dd00dfe0cd3d5fc498b6aa858907bee9b74
Zaai90 <erik.grune@gmail.com>
2021-11-15 21:56:16 +0100

    Fixed error in testdata

--- DML.sql
+++ DML.sql
@@ -39,18 +39,18 @@ INSERT INTO [ConsumableType] ([Id]) Values
 -- SKAPA ITEM
 INSERT INTO [Item] ([Name], Price, ItemType, WeaponType, Slot, [Material], MinDamage, MaxDamage, Defense, AmountToRestore, ConsumableType, DropChance, MaxStack) 
 VALUES 
-('PlateHelmet', 5, 'Armor', null, 'Helmet', 'Plate', null, null, 5, null, null, 20, 1), 
-('PlateChest', 5, 'Armor', null, 'Chest', 'Plate', null, null, 5, null, null, 20, 1),
-('PlateGloves', 5, 'Armor', null, 'Gloves', 'Plate', null, null, 5, null, null, 20, 1),
-('PlateLegs', 5, 'Armor', null, 'Legs', 'Plate', null, null, 5, null, null, 20, 1),
-('PlateBoots', 5, 'Armor', null, 'Boots', 'Plate', null, null, 5, null, null, 20, 1),
-('SupaDupaAxe', 5, 'Weapon', 'DoubleEdgedAxe', 'Weapon', null, 1, 5, null, null, null, 20, 1),
-('Lesser Health Potion', 10, 'Consumable', null, null, null, null, null, null, null, 'HealthPotion', 10, 5),
-('Greater Health Potion', 100, 'Consumable', null, null, null, null, null, null, null, 'HealthPotion', 50, 5),
-('Superior Potion', 500, 'Consumable', null, null, null, null, null, null, null, 'HealthPotion', 300, 5),
-('Lesser Restoring Potion', 10, 'Consumable', null, null, null, null, null, null, null, 'PowerPotion', 10, 5),
-('Greater Restoring Potion', 100, 'Consumable', null, null, null, null, null, null, null, 'PowerPotion', 50, 5),
-('Greater Restoring Potion', 500, 'Consumable', null, null, null, null, null, null, null, 'PowerPotion', 300, 5);
+('PlateHelmet', 5, 'Armor', null, 'Helmet', 'Plate', null, null, 5, null, null, 0, 1), 
+('PlateChest', 5, 'Armor', null, 'Chest', 'Plate', null, null, 5, null, null, 0, 1),
+('PlateGloves', 5, 'Armor', null, 'Gloves', 'Plate', null, null, 5, null, null, 0, 1),
+('PlateLegs', 5, 'Armor', null, 'Legs', 'Plate', null, null, 5, null, null, 0, 1),
+('PlateBoots', 5, 'Armor', null, 'Boots', 'Plate', null, null, 5, null, null, 0, 1),
+('SupaDupaAxe', 5, 'Weapon', 'DoubleEdgedAxe', 'Weapon', null, 1, 5, null, null, null, 0, 1),
+('Lesser Health Potion', 10, 'Consumable', null, null, null, null, null, null, 50, 'HealthPotion', 0, 5),
+('Greater Health Potion', 100, 'Consumable', null, null, null, null, null, null, 100, 'HealthPotion', 0, 5),
+('Superior Potion', 500, 'Consumable', null, null, null, null, null, null, 500 ,'HealthPotion', 0, 5),
+('Lesser Restoring Potion', 10, 'Consumable', null, null, null, null, null, null, 50, 'PowerPotion', 0, 5),
+('Greater Restoring Potion', 100, 'Consumable', null, null, null, null, null, null, 100, 'PowerPotion', 0, 5),
+('Greater Restoring Potion', 500, 'Consumable', null, null, null, null, null, null, 500, 'PowerPotion', 0, 5);
 -- ('Gold', 1, 'Consumable', null, null, null, null, null, 5, null, null, 60, 10000000)
 
 
